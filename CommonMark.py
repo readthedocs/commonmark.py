ebde1fa4f4942636ef30cbc855e0ee888d9c73dc
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-16 01:43:58 -0700

    possibly fixing self.match...

--- CommonMark.py
+++ CommonMark.py
@@ -99,7 +99,7 @@ class InlineParser(object):
 		self.refmap = {}
 
 	def match(self, regexString, reCompileFlags):
-		regex = re.compile(regexString, flags=reCompileFlags)
+		#regex = re.compile(regexString, flags=reCompileFlags)
 		match = regex.search(subject, pos)
 		if match:
 			self.pos = match.end()
@@ -114,18 +114,18 @@ class InlineParser(object):
 			return None
 
 	def spnl(self):
-		self.match(r"^ *(?:\n *)?")
+		self.match(re.compile("^ *(?:\n *)?"))
 		return 1
 
 	def parseBackticks(self, inlines):
 		startpos = self.pos
-		ticks = self.match("^`+")
+		ticks = self.match(re.compile("^`+"))
 		if not ticks:
 			return 0
 		afterOpenTicks = this.pos
 		foundCode = false
 		match = None
-		while ((not foundCode) and (match == self.match("`+", re.MULTILINE))):
+		while ((not foundCode) and (match == self.match(re.compile("`+", [re.MULTILINE])))):
 			if (match == ticks):
 				c = self.subject[afterOpenTicks:(self.pos-len(ticks))]
 				c = re.sub(r"[ \n]+", ' ', c)
@@ -157,8 +157,8 @@ class InlineParser(object):
 			return 0
 
 	def parseAutoLink(self, inlines):
-		m = self.match("^<([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>")
-		m2 = self.match("^<(?:coap|doi|javascript|aaa|aaas|about|acap|cap|cid|crid|data|dav|dict|dns|file|ftp|geo|go|gopher|h323|http|https|iax|icap|im|imap|info|ipp|iris|iris.beep|iris.xpc|iris.xpcs|iris.lwz|ldap|mailto|mid|msrp|msrps|mtqp|mupdate|news|nfs|ni|nih|nntp|opaquelocktoken|pop|pres|rtsp|service|session|shttp|sieve|sip|sips|sms|snmp|soap.beep|soap.beeps|tag|tel|telnet|tftp|thismessage|tn3270|tip|tv|urn|vemmi|ws|wss|xcon|xcon-userid|xmlrpc.beep|xmlrpc.beeps|xmpp|z39.50r|z39.50s|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|chrome|chrome-extension|com-eventbrite-attendee|content|cvs|dlna-playsingle|dlna-playcontainer|dtn|dvb|ed2k|facetime|feed|finger|fish|gg|git|gizmoproject|gtalk|hcp|icon|ipn|irc|irc6|ircs|itms|jar|jms|keyparc|lastfm|ldaps|magnet|maps|market|message|mms|ms-help|msnim|mumble|mvn|notes|oid|palm|paparazzi|platform|proxy|psyc|query|res|resource|rmi|rsync|rtmp|secondlife|sftp|sgn|skype|smb|soldat|spotify|ssh|steam|svn|teamspeak|things|udp|unreal|ut2004|ventrilo|view-source|webcal|wtai|wyciwyg|xfire|xri|ymsgr):[^<>\x00-\x20]*>", "i") 
+		m = self.match(re.compile("^<([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>"))
+		m2 = self.match(re.compile("^<(?:coap|doi|javascript|aaa|aaas|about|acap|cap|cid|crid|data|dav|dict|dns|file|ftp|geo|go|gopher|h323|http|https|iax|icap|im|imap|info|ipp|iris|iris.beep|iris.xpc|iris.xpcs|iris.lwz|ldap|mailto|mid|msrp|msrps|mtqp|mupdate|news|nfs|ni|nih|nntp|opaquelocktoken|pop|pres|rtsp|service|session|shttp|sieve|sip|sips|sms|snmp|soap.beep|soap.beeps|tag|tel|telnet|tftp|thismessage|tn3270|tip|tv|urn|vemmi|ws|wss|xcon|xcon-userid|xmlrpc.beep|xmlrpc.beeps|xmpp|z39.50r|z39.50s|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|chrome|chrome-extension|com-eventbrite-attendee|content|cvs|dlna-playsingle|dlna-playcontainer|dtn|dvb|ed2k|facetime|feed|finger|fish|gg|git|gizmoproject|gtalk|hcp|icon|ipn|irc|irc6|ircs|itms|jar|jms|keyparc|lastfm|ldaps|magnet|maps|market|message|mms|ms-help|msnim|mumble|mvn|notes|oid|palm|paparazzi|platform|proxy|psyc|query|res|resource|rmi|rsync|rtmp|secondlife|sftp|sgn|skype|smb|soldat|spotify|ssh|steam|svn|teamspeak|things|udp|unreal|ut2004|ventrilo|view-source|webcal|wtai|wyciwyg|xfire|xri|ymsgr):[^<>\x00-\x20]*>", [re.IGNORECASE])) 
 		if m:
 			# email
 			dest = m[1:-1]
@@ -314,7 +314,7 @@ class InlineParser(object):
 		pass
 
 	def parseEntity(self, inlines):
-		m = self.match("^&(?:#x[a-f0-9]{1,8}|#[0-9]{1,8}|[a-z][a-z0-9]{1,31});", [re.IGNORECASE])
+		m = self.match(re.compile("^&(?:#x[a-f0-9]{1,8}|#[0-9]{1,8}|[a-z][a-z0-9]{1,31});", [re.IGNORECASE]))
 		if m:
 			inlines.append(Block(t="Entity", c=m))
 			return len(m)
