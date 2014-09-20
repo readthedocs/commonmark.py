ec9746abb7e93e63c75a76ffb8ba4e88d2d2e117
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-20 01:15:09 -0700

    added -d flag for tracing calls in CommonMark.py

--- CommonMark-tests.py
+++ CommonMark-tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re, time, codecs, argparse
+import re, time, codecs, argparse, sys
 import pprint
 import CommonMark
 
@@ -12,11 +12,26 @@ class colors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+def trace_calls(frame, event, arg):
+	co = frame.f_code
+	func_name = co.co_name
+	if func_name == "write":
+		return
+	line_no = frame.f_lineno
+	filename = co.co_filename
+	if event == "call" and not re.match("__", func_name) and re.search("CommonMark", filename):
+		print("Called "+func_name+" at "+str(line_no)+" in "+filename)
+		return trace_calls
+
 parser = argparse.ArgumentParser(description="script to run the CommonMark specification tests against the CommonMark.py parser")
 parser.add_argument('-t', help="Single test to run or comma seperated list of tests (-t 10 or -t 10,11,12,13)")
 parser.add_argument('-v', action="store_true", help="Verbose mode for debugging, print more stuff...")
 parser.add_argument('-i', action="store_true", help="Interactive Markdown input mode")
+parser.add_argument('-d', action="store_true", help="Debug stuff")
 args = parser.parse_args()
+
+if args.d:
+	sys.settrace(trace_calls)
 
 renderer = CommonMark.HTMLRenderer()
 parser = CommonMark.DocParser()
