3cfb8b0e889b5466296fcedf45d5be808d9ae045
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-26 08:57:50 -0700

    i always forget what is and isn't in future :<

--- CommonMark/__init__.py
+++ CommonMark/__init__.py
@@ -1,12 +1,6 @@
-import sys
+from __future__ import absolute_import
 __all__ = ["HTMLRenderer", "DocParser", "dumpAST", "ASTtoJSON"]
-if sys.version_info < (3,0):
-	from CommonMark import HTMLRenderer
-	from CommonMark import DocParser
-	from CommonMark import dumpAST
-	from CommonMark import ASTtoJSON
-else:
-	from CommonMark.CommonMark import HTMLRenderer
-	from CommonMark.CommonMark import DocParser
-	from CommonMark.CommonMark import dumpAST
-	from CommonMark.CommonMark import ASTtoJSON
+from CommonMark.CommonMark import HTMLRenderer
+from CommonMark.CommonMark import DocParser
+from CommonMark.CommonMark import dumpAST
+from CommonMark.CommonMark import ASTtoJSON
