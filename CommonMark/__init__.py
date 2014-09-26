8a224bbacb799958c5c80a0bdd1dd313d72fb7d8
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-26 08:43:21 -0700

    that's better...

--- CommonMark/__init__.py
+++ CommonMark/__init__.py
@@ -1,6 +1,12 @@
+import sys
 __all__ = ["HTMLRenderer", "DocParser", "dumpAST", "ASTtoJSON"]
-from CommonMark import HTMLRenderer
-from CommonMark import DocParser
-from CommonMark import dumpAST
-from CommonMark import ASTtoJSON
-
+if sys.version_info < (3,0):
+	from CommonMark import HTMLRenderer
+	from CommonMark import DocParser
+	from CommonMark import dumpAST
+	from CommonMark import ASTtoJSON
+else:
+	from CommonMark.CommonMark import HTMLRenderer
+	from CommonMark.CommonMark import DocParser
+	from CommonMark.CommonMark import dumpAST
+	from CommonMark.CommonMark import ASTtoJSON
