267680a4dee21936a3703725dae4799603386399
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-26 08:38:06 -0700

    will this work?

--- CommonMark/__init__.py
+++ CommonMark/__init__.py
@@ -1,1 +1,6 @@
 __all__ = ["HTMLRenderer", "DocParser", "dumpAST", "ASTtoJSON"]
+from CommonMark import HTMLRenderer
+from CommonMark import DocParser
+from CommonMark import dumpAST
+from CommonMark import ASTtoJSON
+
