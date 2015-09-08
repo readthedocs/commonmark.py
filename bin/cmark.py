bd18c9a168193bb7a5f7109304b34df4de2bfc6e
Nik Nyby <nikolas@gnu.org>
2015-09-08 19:21:38 -0400

    flake8 fixes for exe file

--- bin/cmark.py
+++ bin/cmark.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
-import argparse, sys
+import argparse
+import sys
 import CommonMark
-parser = argparse.ArgumentParser(description="Process Markdown according to the CommonMark specification.")
+parser = argparse.ArgumentParser(
+    description="Process Markdown according to the CommonMark specification.")
 if sys.version_info < (3, 0):
     reload(sys)
     sys.setdefaultencoding('utf-8')
-parser.add_argument('infile', nargs="?", type=argparse.FileType('r'), default=sys.stdin, help="Input Markdown file to parse, defaults to STDIN")
-parser.add_argument('-o', nargs="?", type=argparse.FileType('w'), default=sys.stdout, help="Output HTML/JSON file, defaults to STDOUT")
+parser.add_argument(
+    'infile',
+    nargs="?",
+    type=argparse.FileType('r'),
+    default=sys.stdin,
+    help="Input Markdown file to parse, defaults to STDIN")
+parser.add_argument(
+    '-o',
+    nargs="?",
+    type=argparse.FileType('w'),
+    default=sys.stdout,
+    help="Output HTML/JSON file, defaults to STDOUT")
 parser.add_argument('-a', action="store_true", help="Print formatted AST")
 parser.add_argument('-aj', action="store_true", help="Output JSON AST")
 args = parser.parse_args()
@@ -27,6 +39,6 @@ if args.a:
     CommonMark.dumpAST(ast)
     exit()
 
-#o.write(ast.to_JSON())
+# o.write(ast.to_JSON())
 o.write(CommonMark.ASTtoJSON(ast))
 exit()
