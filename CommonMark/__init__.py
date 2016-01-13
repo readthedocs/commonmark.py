91bcbd0c575367aedc42ef5609b4cbdc2a7e9eac
nsg <nsg@nocsrv1.net.ic.ac.uk>
2016-01-13 20:03:47 +0000

    flake8 didn't like the `__all__` above the imports

--- CommonMark/__init__.py
+++ CommonMark/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
-__all__ = ["HtmlRenderer", "Parser", "dumpAST", "ASTtoJSON", "commonmark"]
 from CommonMark.CommonMark import HtmlRenderer
 from CommonMark.CommonMark import Parser
 from CommonMark.CommonMark import dumpAST
 from CommonMark.CommonMark import ASTtoJSON
 from CommonMark.CommonMark import commonmark
+__all__ = ["HtmlRenderer", "Parser", "dumpAST", "ASTtoJSON", "commonmark"]
