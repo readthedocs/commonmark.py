493544b96a34ea2a016ac0ed6eb23edc91f89a59
Nik Nyby <nnyby@columbia.edu>
2018-01-03 14:10:08 -0500

    update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CommonMark-py'
-copyright = '2014-2017, Roland Shoemaker, Bibek Kafle, Nik Nyby'
+copyright = '2014-2018, Roland Shoemaker, Bibek Kafle, Nik Nyby'
 author = 'Roland Shoemaker, Bibek Kafle, Nik Nyby'
 
 # The version info for the project you're documenting, acts as replacement for
